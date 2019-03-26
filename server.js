8110857ab8ffec088aa69961b6a9841e36a4bf3e
vaibhav <karnam.v@husky.neu.edu>
2019-03-26 13:48:16 -0400

    cors

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin",
-        "https://http://localhost:4200");
+        "http://localhost:4200");
     res.header("Access-Control-Allow-Headers",
         "Origin, X-Requested-With, Content-Type, Accept");
     res.header("Access-Control-Allow-Methods",
