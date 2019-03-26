c8f8df46003e7c6c6912c96efa4f6fc467899f2d
vaibhav <karnam.v@husky.neu.edu>
2019-03-26 13:12:46 -0400

    cors

--- server.js
+++ server.js
@@ -18,6 +18,16 @@ app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Credentials", "true");
     next();
 });
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin",
+        "https://husky-neighborhood.herokuapp.com");
+    res.header("Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods",
+        "GET, POST, PUT, DELETE, OPTIONS");
+    res.header("Access-Control-Allow-Credentials", "true");
+    next();
+});
 
 app.set('view engine', 'ejs');
 // require("./utilities/filelist");
